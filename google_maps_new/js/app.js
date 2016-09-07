cc0a7c6a60ec07d048dd6461050e11ef8f61f0f6
chonim <chiho1jjan@naver.com>
2016-09-07 20:06:14 +0900

    deleted marker icon

--- google_maps_new/js/app.js
+++ google_maps_new/js/app.js
@@ -96,13 +96,13 @@ function initMap() {
           map.setCenter(place.geometry.location);
           map.setZoom(17);  // Why 17? Because it looks good.
         }
-        marker.setIcon(/** @type {google.maps.Icon} */({
-          url: place.icon,
-          size: new google.maps.Size(71, 71),
-          origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(17, 34),
-          scaledSize: new google.maps.Size(35, 35)
-        }));
+        // marker.setIcon(/** @type {google.maps.Icon} */({
+        //   url: place.icon,
+        //   size: new google.maps.Size(71, 71),
+        //   origin: new google.maps.Point(0, 0),
+        //   anchor: new google.maps.Point(17, 34),
+        //   scaledSize: new google.maps.Size(35, 35)
+        // }));
         marker.setPosition(place.geometry.location);
         marker.setVisible(true);
 
